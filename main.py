5200daef9dd5fe2a2d282bfe3b243212f263ffc9
windowsnipz <nstrickland2014@gmail.com>
2024-02-07 17:18:37 -0500

    Updates display in the game loop

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Zomboid: Rosewood Rising")
 pygame.display.set_icon(pygame.image.load("Assets/spiffo.png"))
 
+
 def main():
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-
+        pygame.display.update()
+    
+    
     pygame.quit()
 
 
