fd9bba52f652cf15e4e9dc574ed6d0a52b3ed828
windowsnipz <nstrickland2014@gmail.com>
2024-02-07 17:24:38 -0500

    Sets the display framerate to 60 FPS

--- main.py
+++ main.py
@@ -5,13 +5,18 @@ WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Zomboid: Rosewood Rising")
 pygame.display.set_icon(pygame.image.load("Assets/spiffo.png"))
 
+FPS = 60
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+        
         pygame.display.update()
     
     
