0ea7caf3e2820d89bf31038459555144e201d607
windowsnipz <nstrickland2014@gmail.com>
2024-02-07 14:15:56 -0500

    Adds spiffo icon to game window

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame
 SCREEN_WIDTH, SCREEN_HEIGHT = 900, 500
 WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Zomboid: Rosewood Rising")
+pygame.display.set_icon(pygame.image.load("Assets/spiffo.png"))
 
 def main():
     run = True
