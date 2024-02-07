53295f8be8d645bfd63d5114cd2daab791e27c82
windowsnipz <nstrickland2014@gmail.com>
2024-02-07 18:14:20 -0500

    Loads heart.png and adds os.path functionality

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pygame
+import os
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 900, 500
 WIN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Zomboid: Rosewood Rising")
-pygame.display.set_icon(pygame.image.load("Assets/spiffo.png"))
+pygame.display.set_icon(pygame.image.load(os.path.join("Assets", "spiffo.png")))
 
 FPS = 60
+HEART = pygame.image.load(os.path.join("Assets", "heart.png"))
 
 def main():
     clock = pygame.time.Clock()
     run = True
     while run:
+
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
