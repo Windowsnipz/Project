c587991eb3e7a0235d9176def1729e8b8791905a
windowsnipz <nstrickland2014@gmail.com>
2024-02-07 20:19:26 -0500

    Puts health bar in a list with image and tuple coords

--- main.py
+++ main.py
@@ -9,14 +9,13 @@ pygame.display.set_icon(pygame.image.load(os.path.join("Assets", "spiffo.png")))
 FPS = 60
 HEART = pygame.image.load(os.path.join("Assets", "heart.png"))
 HEART_EMPTY = pygame.image.load(os.path.join("Assets", "heart_empty.png"))
+hearts = [(HEART, (360, 400)), (HEART, (390, 400)), (HEART, (420, 400)), 
+          (HEART, (450, 400)), (HEART, (480, 400))]
 
 def draw_window():
     WIN.fill((0, 0, 0))
-    WIN.blit(HEART, (360, 400))
-    WIN.blit(HEART, (390, 400))
-    WIN.blit(HEART, (420, 400))
-    WIN.blit(HEART_EMPTY, (450, 400))
-    WIN.blit(HEART_EMPTY, (480, 400))
+    for heart, pos in hearts:
+        WIN.blit(heart, pos)
     pygame.display.update()
 
 def main():
