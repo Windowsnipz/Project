fece3a3cb754dd2f418e26e558ea1ecb150d6f9e
windowsnipz <nstrickland2014@gmail.com>
2024-02-14 18:38:41 -0500

    More progress on path1

--- story_paths/story_path1.py
+++ story_paths/story_path1.py
@@ -21,7 +21,8 @@ def path1():
     
     printcool("While coming down the stairs, all seems eerily quiet...", "Overcast weather was evident through the pale curtains of the living room windows.")
     
-    printcool("However..."), wait(), printcool("The atmosphere was not the only thing that carried a pale demeanor.\n"), wait()
+    printcool("However...", "The atmosphere was not the only thing that carried a pale demeanor.")
+    
     printcool(f"Lying fetal under the coffee table was {protagonist.name}'s daughter, Emily."), wait()
     printcool(f"Softly call Emily's name, or sneak away?\n")
     choice = input()
