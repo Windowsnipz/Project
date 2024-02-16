3e1c7362553f932527ff44045aeda161859c5e56
windowsnipz <nstrickland2014@gmail.com>
2024-02-15 19:48:07 -0500

    More story, more problems

--- encounters/ecounter1.py
+++ encounters/ecounter1.py
@@ -3,3 +3,6 @@ from helpers import printcool
 #TODO: add encounter1
 
 printcool("Suddenly, Emily looks up at the sky.", "Her eyes are bloodshot and glazed over.")
+
+printcool("It all happened so fast.", f"She stood up so quickly, {protagonist.name} couldn't believe it.")
+# TODO fix code dependency!!!! :(
