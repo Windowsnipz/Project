dc8b52628740acd43028d0646a83d6806a9f5850
windowsnipz <nstrickland2014@gmail.com>
2024-02-17 17:57:48 -0500

    grammar

--- encounters.py
+++ encounters.py
@@ -31,23 +31,23 @@ def encounters(protagonist):
 
 # Rosewood Medical encounter (broken collarbone?)
 def encounter1(protagonist):
-    printcool("Noah was squirming in the doctor's office of Rosewood Medical.", "\"I knew I shouldn't have gone skiing today\"")
+    printcool("Noah was squirming in the doctor's office of Rosewood Medical.", "\"I knew I shouldn't have gone skiing today.\"")
 
-    printcool("\"Don't put too much pressure on yourself. This break will heal on its own\" muttered the doctor.", "His lips curled, and he started to sniffle.")
+    printcool("\"Don't put too much pressure on yourself. This break will heal on its own,\" muttered the doctor.", "His lips curled, and he started to sniffle.")
 
     printcool("\"What's wrong?\" asked the doctor, \"I thought you said there wasn't much pain?\"")
-    printcool("\"You don't understand\" he grumbled.", "\"There's no possible way I can afford the medical bill.\"")
+    printcool("\"You don't understand,\" he grumbled.", "\"There's no possible way I can afford the medical bill.\"")
 
     printcool("\"I'm sorry, sir, but we can't do anything about that at the moment.", "You should be focusing on getting better.\"")
     printcool("Noah was discharged, and plopped into a seat in the waiting room.", "\" I cant believe I went to the top of the mountain. I'm just a beginner!\"")
 
     printcool(f"{protagonist.name} burst through the doors of the clinic.", "\"HELP! HELP ME! There's something going on out there!\"")
-    printcool("The shocked receptionist stared at them, mouth agape", "\"What do you mean? Are you hurt?\"")
+    printcool("The shocked receptionist stared at them, mouth agape.", "\"What do you mean? Are you hurt?\"")
 
     printcool("A loud crash resounded from the back of the clinic, followed by a scream", "\"Hey!! What's going on?!\"")
     printcool("Bodies begain to fill the hallway. Bodies that weren't human.", "Horrific sounds echoed from their mouths.")
 
-    printcool("\"You have got to be kidding me!\" Noah exclaimed.", "\"Just eat me. I've had enough\"")
+    printcool("\"You have got to be kidding me!\" Noah exclaimed.", "\"Just eat me. I've had enough...\"")
     printcool(f"{protagonist.name} started to panic.", "Fight or run away?")
     fight = get_input("fight")
 
