676c934a4f758a8e31e54839b3217eab2e74ec30
windowsnipz <nstrickland2014@gmail.com>
2024-02-17 11:21:14 -0500

    added to encounters

--- encounters.py
+++ encounters.py
@@ -1,4 +1,17 @@
 # Loops player encounters until player dies :(
 import random
 
-from helpers import Zombie, printcool, get_character_name, random_weapon, printgloom, get_input
+from helpers import Zombie, printcool, random_weapon, get_input
+
+def encounters():
+    num = random.randint(1, 5)
+    if num == 1:
+        encounter1()
+    if num == 2:
+        encounter2()
+    if num == 3:
+        encounter3()
+    if num == 4:
+        encounter4()
+    if num == 5:
+        encounter5()
