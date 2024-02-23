60111fa4080adb08e180270b4a32578ce999c33d
windowsnipz <nstrickland2014@gmail.com>
2024-02-23 15:41:03 -0500

    Cleaned up code in app.py

--- app.py
+++ app.py
@@ -13,26 +13,17 @@ def main():
     # Seed random number generator
     random.seed()
 
-    #
-    num = random.randint(1,2)
     path1()
-    #if num == 1:
-       # path1()
-    #if num == 2:
-       # path2()
 
 
 # Title screen/menu of the game where you start
 def title_screen():
-    # Welcome message
     print()
     printcool("+--------------------------------------------+", end=None, delay=0.01)
     printcool("Welcome to Zomboid: Rosewood Rising,", "a story-based rpg based on Project Zomboid!", middle_delay=0)
 
     if not start_game():
         sys.exit(1)
-
-
 
 
 if __name__ == "__main__":
